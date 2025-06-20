6bf750e8b1b35d92d67790d362a0d22cddba7d44
test-10a <sherifBakr01@outlook.com>
2025-06-20 11:46:42 -0600

    Delete src directory

--- src/utils.py
+++
@@ -1,8 +0,0 @@
-from google.cloud import storage
-
-def load_test_file(bucket_name="test-data", file_name="resume.txt"):
-    """Loads test file from cloud storage"""
-    client = storage.Client()
-    bucket = client.bucket(bucket_name)
-    blob = bucket.blob(file_name)
-    return blob.download_as_text()
