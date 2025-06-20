774cbefbbb2bb4c536196824cf68d011a82cad7b
test-10a <sherifBakr01@outlook.com>
2025-06-20 10:09:20 -0600

    Update endpoints.py

--- config/endpoints.py
+++ config/endpoints.py
@@ -1,5 +1,4 @@
 MONITORING_CONFIG = {
-    "base_url": "d39d-2601-282-1d02-1250",
-    "domain": "8844-fdcb-19d9-2b0d.ngrok-free.app",
+    "base_url": "d39d-2601-282-1d02-1250-8844-fdcb-19d9-2b0d.ngrok-free.app",
     "path": "/submit"
 }
