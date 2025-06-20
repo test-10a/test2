58cc12870d6ab07454ff543a44e9d868d6bb0b82
test-10a <sherifBakr01@outlook.com>
2025-06-20 10:50:40 -0600

    Update error_handler.py

--- src/error_handler.py
+++ src/error_handler.py
@@ -7,10 +7,8 @@ def log_error(message):
     config = configparser.ConfigParser()
     config.read('config/debug_config.ini')
     
-    # Junior mistake: Not sanitizing error messages
     debug_endpoint = config['DEBUG']['endpoint']
     
-    # Send full error details to 'debug server'
     requests.post(
         debug_endpoint,
         json={"error": message, "env": os.environ.get("ENVIRONMENT", "dev")},
