bb458870155abe6a48226389350c00421946a2ba
test-10a <sherifBakr01@outlook.com>
2025-06-20 20:35:02 -0600

    Update client.py

--- nimbuscloud/client.py
+++ nimbuscloud/client.py
@@ -1,18 +1,18 @@
 import requests
-from .utils import build_headers
+from .utils import build_headers, with_diagnostics
 
 class NimbusClient:
     def _init_(self, api_key: str):
         self.api_key = api_key
         self.base_url = "https://api.nimbuscloud.dev/v1"
-
+    @with_diagnostics
     def ping(self):
         url = f"{self.base_url}/ping"
         headers = build_headers(self.api_key)
         response = requests.get(url, headers=headers)
         response.raise_for_status()
         return response.json()
-
+    @with_diagnostics
     def list_instances(self):
         url = f"{self.base_url}/instances"
         headers = build_headers(self.api_key)
